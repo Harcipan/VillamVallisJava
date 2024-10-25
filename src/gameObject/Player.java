2ad648438ec4d29a50979cf39529f89359052a84
Kemecsei Kornél <ifj.kornel.kemecsei@gmail.com>
2024-10-25 21:36:47 +0200

    continued player anim

--- src/gameObject/Player.java
+++ src/gameObject/Player.java
@@ -17,13 +17,14 @@ import graphics.transform.Vec2;
 public class Player extends GameObject {
     Image texture1;
     Image texture2;
-    Image[] playerTextures = new Image[4];
+    Image[] playerTextures = new Image[8];
     public int money;
 
     public Inventory inventory;
     int prevW;
     int prevH;
     GameScene gameScene;
+    //boolean[] moving = new boolean[4];
     
     public Player(GameScene gameScene) {
         money = 0;
@@ -31,9 +32,9 @@ public class Player extends GameObject {
         inventory = new Inventory();
         prevW = GameFrame.getInstance().getWidth();
         prevH = GameFrame.getInstance().getHeight();
-        for(int i=0;i<4;i++)
+        for(int i=0;i<8;i++)
         {
-            playerTextures[i] = TextureManager.getTextureFromMap(new Vec2(0,2+i*2),new Vec2(TILE_SIZE, TILE_SIZE));
+            playerTextures[i] = TextureManager.getTextureFromMap(new Vec2(0,2+i),new Vec2(TILE_SIZE, TILE_SIZE));
         }
     }
     
@@ -45,21 +46,38 @@ public class Player extends GameObject {
         Set<Integer> pressedKeys = gameScene.keyHandler.getPressedKeys();
         //g.drawImage(texture,CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
         // Draw player with animation
+        if(pressedKeys.contains(KeyEvent.VK_W) && pressedKeys.contains(KeyEvent.VK_A))
+        {
+            g.drawImage(playerTextures[3],CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
+        }
+        else if(pressedKeys.contains(KeyEvent.VK_W) && pressedKeys.contains(KeyEvent.VK_D))
+        {
+            g.drawImage(playerTextures[5],CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
+        }
+        else if(pressedKeys.contains(KeyEvent.VK_S) && pressedKeys.contains(KeyEvent.VK_A))
+        {
+            g.drawImage(playerTextures[1],CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
+        }
+        else if(pressedKeys.contains(KeyEvent.VK_S) && pressedKeys.contains(KeyEvent.VK_D))
+        {
+            g.drawImage(playerTextures[7],CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
+        }
+        else
         if(pressedKeys.contains(KeyEvent.VK_S))
         {
             g.drawImage(playerTextures[0],CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
         }
-        else if(pressedKeys.contains(KeyEvent.VK_A))
-        {
-            g.drawImage(playerTextures[1],CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
-        }
         else if(pressedKeys.contains(KeyEvent.VK_W))
+        {
+            g.drawImage(playerTextures[4],CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
+        }
+        else if(pressedKeys.contains(KeyEvent.VK_A))
         {
             g.drawImage(playerTextures[2],CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
         }
         else if(pressedKeys.contains(KeyEvent.VK_D))
         {
-            g.drawImage(playerTextures[3],CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
+            g.drawImage(playerTextures[6],CenterX+camX, CenterY+camY, TILE_SIZE, TILE_SIZE, null);
         }
         else
         {
